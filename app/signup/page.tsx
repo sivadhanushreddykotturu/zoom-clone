4b4e1768a012ea630200eeddf9037c6e3c4c2eda
scavengerui <ksdhanushreddy@gmail.com>
2026-08-01 16:44:30 +0530

    feat: add dedicated Clerk signup page and fix signup redirect loop

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,15 +1,15 @@
 'use client'
 
-import { SignIn } from '@clerk/nextjs'
+import { SignUp } from '@clerk/nextjs'
 import { Footer } from '@/components/footer'
 
-export default function LoginPage() {
+export default function SignupPage() {
   return (
     <div className="flex min-h-dvh flex-col items-center justify-center bg-black px-4 text-zinc-100">
       <div className="flex flex-1 items-center justify-center py-12">
-        <SignIn
+        <SignUp
           routing="hash"
-          signUpUrl="/signup"
+          signInUrl="/login"
           forceRedirectUrl="/dashboard"
         />
       </div>
